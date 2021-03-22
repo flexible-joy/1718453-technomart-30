1f68729ab005b9dd13b9ff8215260fcc55dab6ca
flexible-joy <77239305+flexible-joy@users.noreply.github.com>
2021-03-22 21:23:05 +0300

    Оживляем интерактивные элементы

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
 const formBtn = document.querySelector(".address-button");
-const modalForm = document.querySelector(".modal-window");
-const commentClose = modalForm.querySelector(".modal-close-comment");
-
+const modalWindow = document.querySelector(".modal-window");
+const commentClose = modalWindow.querySelector(".modal-close-comment");
+const modalForm = modalWindow.querySelector(".modal-form");
+const commentName = modalWindow.querySelector(".enter-name");
+const commentMail = modalWindow.querySelector(".enter-mail");
 
 formBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalForm.classList.add("active");
+  modalWindow.classList.add("active");
+  commentName.focus();
 
 });
 
 commentClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalForm.classList.remove("active");
+  modalWindow.classList.remove("active");
+  modalWindow.classList.remove("error");
+});
+
+
+modalForm.addEventListener("submit", function (evt) {
+  if (!commentName.value || !commentMail.value) {
+    evt.preventDefault();
+    modalWindow.classList.remove("error");
+    modalWindow.offsetWidth = modalWindow.offsetWidth;
+    modalWindow.classList.add("error");
+
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalWindow.classList.contains("active")) {
+      evt.preventDefault();
+      modalWindow.classList.remove("active");
+      modalWindow.classList.add("error");
+    }
+  }
 });
